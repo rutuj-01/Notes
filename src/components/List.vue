2d49dbef79aa7aaab2cb286704481be450ccbd86
Rutuj Bhakre <rbhakre99@gmail.com>
2020-12-26 11:01:04 +0530

    completed the counter part with list appending inputs

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,35 +1,69 @@
 
 <template>
 	<div class="container">
-	<b-alert show >Primary Alert</b-alert>
-	<b-button variant="primary" style="margin-left: 450px;">
+	<!-- <b-alert show >Primary Alert</b-alert> -->
+	<b-button variant="primary" style="margin-left: 450px; margin-top:30px;">
     Profile
-    <b-badge variant="light" >9 <span class="sr-only">unread messages</span></b-badge>
+    <b-badge variant="light" >{{ counter }}</b-badge>
 	</b-button>
 	<b-form >
       <b-form-group
-        id="input-group-1"
-        label="item"
+        label="Item"
       >
         <b-form-input
           id="input-1"
           type="text"
           placeholder="Enter item to add"
-   
+			v-model="name"
         ></b-form-input>
       </b-form-group>
      </b-form>
-	<b-button variant="success" style="margin-left: 450px;">+Add</b-button>
+	<b-button variant="success" style="margin-left: 450px;" @click.prevent= "add()" :disabled="check" >+Add</b-button>
 	<br>
 	List items
+	
 	<b-list-group>
-	<b-list-group-item>Cras justo odio</b-list-group-item>
+	<b-list-group-item v-for="(item,index) in items" :key="index" style="text-align: center;">
+		{{ item }}
+
+	</b-list-group-item>
 	</b-list-group>
-
-
 	</div>
 
 </template>
 <script >
-		
+		export default{
+			data(){
+				return { 
+					name : '',
+					
+					items: []
+				}
+			},
+			methods:
+			{
+				add(){
+
+					this.items.push(this.name)
+					this.name=''
+					// alert(this.name)
+				}
+			},
+			computed:
+			{	
+				counter(){
+					return this.items.length
+				},
+				check()
+				{	
+					if(this.name.length != 0){
+						return false
+					}
+					else{
+						return true
+					}
+					
+				}
+			}
+		}
 </script>
